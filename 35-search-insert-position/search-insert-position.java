62286584b1b9728c595519f3e036718c6417e7d7
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-07-24 01:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.39%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        int ans = nums.length;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(target<=nums[mid]){
+                ans=mid;
+                high = mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
