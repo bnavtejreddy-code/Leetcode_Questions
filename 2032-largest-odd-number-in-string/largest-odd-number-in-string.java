4cf2b38e1aadc958928f3e59760081d1d45f29f6
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-07-30 01:25:35 +0530

    Time: 1 ms (99.78%) | Memory: 47.1 MB (37.05%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int i ;
+        int ind=-1;
+        for( i = num.length()-1;i>=0;i--){
+            if((num.charAt(i)-'0')%2 == 1){
+                ind=i;
+                break;
+            }
+            
+        }
+        if(ind==-1){
+            return "";
+        }
+        return num.substring(0,ind+1);
+        
+    }
+}
