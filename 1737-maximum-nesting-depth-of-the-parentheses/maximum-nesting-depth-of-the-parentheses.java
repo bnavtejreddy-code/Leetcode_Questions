e7ddc3568dfcc761b76871278a324c1932dac5a3
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-01 00:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (94.66%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0;
+        int max = 0;
+        for(char ch : s.toCharArray()){
+            if(ch=='('){
+                count++;
+                max= Math.max(count,max);
+            }else if(ch==')'){
+                count--;
+            }
+        }
+        return max;
+    }
+}
