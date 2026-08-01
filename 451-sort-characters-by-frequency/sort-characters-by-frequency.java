c2e093ff6a9ab5bb7dbc4137ce504924d128c649
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-01 10:29:53 +0530

    Time: 3 ms (99.78%) | Memory: 46.7 MB (52.58%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,49 +1,31 @@
 class Solution {
-
     public String frequencySort(String s) {
-
-        // Result array to store the final answer
+        
+        int[] freq=new int[128];
         char[] res = new char[s.length()];
 
-        // Frequency array for all ASCII characters
-        int[] freq = new int[128];
-
-        // Count the frequency of every character
-        for (char ch : s.toCharArray()) {
+        for(char ch : s.toCharArray()){
             freq[ch]++;
         }
-
-        // Index where we will insert characters into the result
         int ind = 0;
-
-        // Continue until the result array is completely filled
-        while (ind < s.length()) {
-
-            // Stores the maximum frequency found
+        while(ind<s.length()){
             int max = 0;
-
-            // Stores the character having the maximum frequency
-            char maxch = 0;
-
-            // Find the character with the highest frequency
-            for (int i = 0; i < 128; i++) {
-
-                if (freq[i] > max) {
-                    max = freq[i];
-                    maxch = (char) i;
+            char maxch=0 ;
+            for(int i =0;i<freq.length;i++){
+                if(max<freq[i]){
+                    max=freq[i];
+                    maxch = (char) i ;
                 }
             }
 
-            // Place the character 'maxch' exactly 'max' times
-            while (max-- > 0) {
-                res[ind++] = maxch;
-            }
-
-            // Mark this character as processed
-            freq[maxch] = 0;
+        
+        while(max-->0){
+            res[ind++]=maxch;
         }
 
-        // Convert character array into String
+        freq[maxch] =0;
+        }
         return new String(res);
     }
+    
 }
