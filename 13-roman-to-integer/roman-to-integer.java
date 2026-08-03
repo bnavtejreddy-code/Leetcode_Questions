5bed930b6423483b6aa58a8dceeea69ddb7559e2
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-03 13:08:26 +0530

    Time: 2 ms (100.00%) | Memory: 46.2 MB (94.04%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,44 +1,31 @@
 class Solution {
-
     public int romanToInt(String s) {
+        int sum = 0;
 
-        // Stores value of every Roman character
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        int ans = 0;
-
-        // Traverse till second last character
         for (int i = 0; i < s.length() - 1; i++) {
+            int curr = convert(s.charAt(i));
+            int next = convert(s.charAt(i + 1));
 
-            // Current Roman value
-            int current = map.get(s.charAt(i));
-
-            // Next Roman value
-            int next = map.get(s.charAt(i + 1));
-
-            // Smaller value before larger value
-            // Example: IV, IX, XL
-            if (current < next) {
-                ans -= current;
-            }
-
-            // Normal case
-            else {
-                ans += current;
+            if (curr < next) {
+                sum -= curr;
+            } else {
+                sum += curr;
             }
         }
 
-        // Last character is always added
-        ans += map.get(s.charAt(s.length() - 1));
+        sum += convert(s.charAt(s.length() - 1));
 
-        return ans;
+        return sum;
+    }
+
+    static int convert(char ch) {
+        if (ch == 'I') return 1;
+        if (ch == 'V') return 5;
+        if (ch == 'X') return 10;
+        if (ch == 'L') return 50;
+        if (ch == 'C') return 100;
+        if (ch == 'D') return 500;
+        if (ch == 'M') return 1000;
+        return 0;
     }
 }
