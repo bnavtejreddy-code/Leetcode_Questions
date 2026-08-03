0b59c6bc15d799c900f7210746e118a56fc22353
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-03 11:08:54 +0530

    Time: 5 ms (61.53%) | Memory: 43.7 MB (99.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,10 +6,9 @@ class Solution {
         }
         for(int i =0;i<s.length();i++){
             freq[s.charAt(i) - 'a']++;
-        }
-        for(int i =0;i<t.length();i++){
             freq[t.charAt(i) - 'a']--;
         }
+        
         for(int i =0;i<freq.length;i++){
             if(freq[i]!=0){
                 return false;
