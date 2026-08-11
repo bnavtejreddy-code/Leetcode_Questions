8029f92e1ed826253ed3db2750ea70ef063fe096
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-11 17:46:59 +0530

    Time: 61 ms (19.43%) | Memory: 48.2 MB (5.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,47 @@
+import java.util.*;
+
 class Solution {
+
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hash = new HashSet<>();
-        if(s.isEmpty()){
-            return 0;
-        }
-        int i =0;
-        int j =0;
-        int max = Integer.MIN_VALUE;
-        while(j<s.length()){
-            char ch = s.charAt(j);
-            while(hash.contains(ch)){
-                hash.remove(s.charAt(i));
-                i++;
+
+        // HashSet stores the characters currently
+        // present inside our sliding window.
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        // right expands the window
+        for (int right = 0; right < s.length(); right++) {
+
+            char current = s.charAt(right);
+
+            // If current character already exists,
+            // the window has a duplicate.
+            //
+            // Remove characters from the left until
+            // the duplicate is removed.
+            while (set.contains(current)) {
+
+                set.remove(s.charAt(left));
+                left++;
             }
-                hash.add(s.charAt(j));
-                max = Math.max(max,j-i+1);
-                j++;
+
+            // Now current character is not duplicated,
+            // so add it to the window.
+            set.add(current);
+
+            // Current window:
+            //
+            // left ........ right
+            //
+            // Length = right - left + 1
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
         }
-        return max;
+
+        return maxLength;
     }
 }
