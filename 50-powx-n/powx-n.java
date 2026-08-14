42fc2a0b2ded3ad02321b9fad7fc7c79c3fc5d8a
bnavtejreddy-code <bnavtejreddy.code@gmail.com>
2026-08-14 14:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (11.73%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,34 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+
+        // Convert n to long because -2147483648
+        // cannot be safely converted to positive int.
+        long power = n;
+
+        // Handle negative exponent
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        // Result starts at 1 because x^0 = 1
+        double result = 1;
+
+        // Binary exponentiation
+        while (power > 0) {
+
+            // If power is odd, use the current x
+            if (power % 2 == 1) {
+                result = result * x;
+            }
+
+            // Square x for the next step
+            x = x * x;
+
+            // Divide exponent by 2
+            power = power / 2;
+        }
+
+        return result;
     }
 }
